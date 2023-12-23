9c9d1162a80dff1fdb9ff3255694d39fb423d15e
Bhavishya <bhavishyasingla2005@gmail.com>
2023-12-23 20:43:19 +0530

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,14 @@
+print("hello") #normal print
+print(2**3) #power of 2 is 3
+a = 2 #variable
+print(a) #printing variable
+print(type(a)) #type of variable a
+a = "hello" #changing type of variable
+print(len(a)) #printing length of string
+print(a[-1]) #printing only one char // -1 means last index
+print(a[2:]) #printing from 2nd index to last // it includes 2nd index
+print(a[:2]) #printing from 0 index till 1st not including 2
+print(a[1:3]) #printing 1st to 2nd index
+print(a[::]) #print whole sting
+print(a[::2]) #it will jump between elements (start from:till index:jump size)
+print(a[::-1]) #reverse string
