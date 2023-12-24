db07b2de99fd2b1ebf65759d093143e3dd17c89e
Bhavishya <bhavishyasingla2005@gmail.com>
2023-12-24 20:12:19 +0530

    Update Day2.py

---
+++ Day2.py
@@ -0,0 +1,7 @@
+name = "Singla"
+print("Bhavishya "+name) #string concatenation
+name = "Max wax"
+print(10*name) #printing string by multiplyig with a number
+print(name.upper()) #changing to upper case // doesnot affect original string
+#lower to change to lowercase 
+print(name.split())
