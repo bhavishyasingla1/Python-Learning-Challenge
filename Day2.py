704255d7c5073185a66743bfb84876ea8e42aa31
Bhavishya <bhavishyasingla2005@gmail.com>
2023-12-24 20:35:46 +0530

    Update Day2.py

--- Day2.py
+++ Day2.py
@@ -5,3 +5,15 @@ print(10*name) #printing string by multiplyig with a number
 print(name.upper()) #changing to upper case // doesnot affect original string
 #lower to change to lowercase 
 print(name.split())
+
+
+#formatting
+print("This is a string {}".format("Inserted"))
+print("Your Name is {1} {0}".format("Singla","Bhavishya")) #we can formt with index number as well
+print("Your Name is {firstname} {lastname}".format(lastname = "Singla",firstname ="Bhavishya")) #we can use variables
+
+result = 111/2222
+print(result)
+print("Value is {r:55.21}".format(r = result)) #{value:width.precision}
+#other method
+print(f"Value is {result}")
