1cb0f0d2382ac74516cb684c9bdae195d3260ff1
Bhavishya <bhavishyasingla2005@gmail.com>
2023-12-31 20:27:51 +0530

    day 6

---
+++ Day6.py
@@ -0,0 +1,16 @@
+#comparison operators
+print(2==2)
+print(2>=0)
+print(2<=0)
+print('hello'=='bye')
+print('Bye' =='bye')
+print('2'==2)
+print(2.0==2)
+print(3!=3)
+
+#chain comparisons using logical operators
+print(2<3 and 3<5)
+print(2<3 or 3>5)
+
+# not opposite returns
+print(not(1==1))
