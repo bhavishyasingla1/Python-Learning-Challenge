2b65747dafb0deb5c6822d1fc58c4739fd3d1f5f
Bhavishya <bhavishyasingla2005@gmail.com>
2024-01-01 21:18:50 +0530

    Day 7

---
+++ Day7.py
@@ -0,0 +1,29 @@
+#if else statements
+if True:
+    print("It's True") 
+
+if 2<4:
+    print("hello")
+
+hungry = False #change this to see different result
+
+if hungry:
+    print("FEED ME")
+else:
+    print("I am not Hungry")
+
+
+# we can use elif to place conditions between if and else
+if (22<3): 
+    print("true if")
+
+elif (3<4):
+    print("true elif")
+    
+elif (5<4):
+    print("False")
+
+else:
+    print('Its else')
+
+#I made AI chatbot today so check it out plz https://linktr.ee/bhavishyasingla
